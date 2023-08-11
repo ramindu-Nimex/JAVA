dcae9131748f2a9bb51d174e65e956a2166d9937
ramindu-git <ramindunimesh61@gmail.com>
2023-08-11 12:16:17 +0530

    Tutorial 2

--- Tutorial 2/BankAccount.java
+++ Tutorial 2/BankAccount.java
@@ -29,21 +29,31 @@ public class BankAccount {
 	// Method to deposit funds
    public void deposit(double amount) {
       balance += amount;
+		System.out.println("Deposit : " + amount);
+		displayBalance();
    }
     
    // Method to withdraw funds
    public void withdraw(double amount) {
       if (amount <= balance) {
          balance -= amount;
+			System.out.println("Withdraw : " + amount);
       } else {
          System.out.println("currently balance is " + balance + " Pleace withdraw minimum balanace");
       }
+		displayBalance();
     }
     
    // Method to display account details
    public void displayinfo() {
       System.out.println("Account Number: " + accountNumber);
       System.out.println("Account Holder's Name: " + accountHolderName);
-      System.out.println("Balance: " + balance);
+      displayBalance();
+   }
+
+	// Method to display balance
+	public void displayBalance() {
+		System.out.println("Balance: " + balance);
+		System.out.println();
    }
 }
--- Tutorial 2/BankApp.java
+++ Tutorial 2/BankApp.java
@@ -6,16 +6,18 @@ public class BankApp {
 		Scanner sc = new Scanner(System.in);
 		
 		// Create two BankAccount objects and assign values
-		BankAccount account1 = new BankAccount("123456v", "John Doe", 1000.0);
+		BankAccount account1 = new BankAccount("123456v", "Nimesh Ramindu", 1000.0);
       BankAccount account2 = new BankAccount("789101v", "Ramindu Nimesh", 10000.0);
         
       // Call the display method to print account details
       System.out.println("Account 1 Details : ");
       account1.displayinfo();
+      account1.deposit(500.0);
       System.out.println();
         
       System.out.println("Account 2 Details:");
       account2.displayinfo();
+      account2.withdraw(1000.0);
 		
 		sc.close();
 	}
